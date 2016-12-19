37fa1a3f4b305eb5e4ea45ca5590cdb5583b1cab
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2016-12-18 23:01:31 -0600

    Added rest of implementations of Interface w/ TODOs
    In addition, fixed the notion of display dim. and buffer dim.

--- source/src/platform/unix/interface.c
+++ source/src/platform/unix/interface.c
@@ -6,6 +6,9 @@
 #define DISPLAY_WIDTH 160
 #define DISPLAY_HEIGHT 128
 
+#define BUFFER_WIDTH DISPLAY_WIDTH
+#define BUFFER_HEIGHT 90
+
 typedef struct interface_str
 {
     int present;
@@ -24,7 +27,7 @@ typedef struct interface_str
 Interface Interface_New( void )
 {
     Interface interface = malloc(sizeof(Interface_Rec));
-    BM_BufferInit_16(&interface->bufman, interface->buffer, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+    BM_BufferInit_16(&interface->bufman, interface->buffer, BUFFER_WIDTH, BUFFER_HEIGHT);
     return interface;
 }
 
@@ -62,6 +65,72 @@ void Interface_DrawLine( Interface interface, uint32_t x0, uint32_t y0, uint32_t
 void Interface_FillRect( Interface interface, uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint16_t color)
 {
     BM_FillRect_16(x, y, w, h, color, &interface->bufman);
+}
+
+void Interface_DrawVLine(Interface interface, uint32_t x, uint32_t y0, uint32_t y1, uint16_t color)
+{
+    BM_DrawVLine_16(x,y0,y1,color,&interface->bufman);
+}
+
+void Interface_DrawHLine(Interface interface, uint32_t x0, uint32_t y, uint32_t x1, uint16_t color)
+{
+    BM_DrawHLine_16(x,y0,y1,color,&interface->bufman);
+}
+
+void Interface_DirectFillRect(Interface interface, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color) // need to figure out how to deal with this
+{
+    //TODO: SDL code
+}
+
+void Interface_SetCursor(Interface interface, uint32_t x, uint32_t y)
+{
+    //TODO: SDL code
+}
+
+void Interface_OutString(Interface interface, uint8_t* string)
+{
+    //TODO: SDL code
+}
+
+typedef struct interface_str
+{
+    int present;
+    int32_t analog[2];
+    unsigned int a : 1;
+    unsigned int b : 1;
+    unsigned int x : 1;
+    unsigned int y : 1;
+    unsigned int l : 1;
+    unsigned int r : 1;
+
+    uint16_t buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+    buffer16 bufman;
+} Interface_Rec;
+
+
+int32_t Interface_GetAnalog(Interface interface, uint8_t axis)
+{
+    int index = axis - 'x';
+    return index < 1? interface->analog[index] : 0xDEADBEEF;
+}
+
+uint8_t Interface_GetDigital(Interface interface, uint8_t button)
+{
+    switch(button)
+    {
+    case 'a':
+        return interface->a;
+    case 'b':
+        return interface->b;
+    case 'x':
+        return interface->x;
+    case 'y':
+        return interface->y;
+    case 'l':
+        return interface->l;
+    case 'r':
+        return interface->r;
+    }
 }
 
 void Interface_GetInput( Interface interface )
