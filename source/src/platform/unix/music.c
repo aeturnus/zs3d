fa0c6c8fc7ba368032f85225778daddd1fa4b88e
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2016-12-18 23:38:34 -0600

    Added function stubs for music

---
+++ source/src/platform/unix/music.c
@@ -0,0 +1,33 @@
+#include "platform/music.h"
+#include "resources/songs.h"
+#include "resources/sounds.h"
+
+void Music_Init(void)
+{
+    //TODO
+}
+
+void Music_Reset(void)
+{
+    //TODO
+}
+
+void Music_LoadSong(uint8_t songIndex)
+{
+    //TODO
+}
+
+void Music_PlaySong(void)
+{
+    //TODO
+}
+
+void Music_PauseSong(void)
+{
+    //TODO
+}
+
+void Music_PlaySound(uint8_t soundIndex)
+{
+    //TODO
+}
